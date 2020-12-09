b218c073b2dffdafa2d8be6fe73d26577b6d545d
Mason Howe <mason.a.howe@gmail.com>
2020-12-08 18:52:12 -0700

    update fetch handler

--- script.js
+++ script.js
@@ -1,38 +1,46 @@
 let city = prompt("enter your City,", "New York")
 let units = "imperial"
+let apiID = "222e451b939e0663df968edf4cecbd01"
+let TempDataStore = null
 
 
-getCityWeather(city,units)
+
+getCityWeather(city, units)
 
 
 function getCityWeather(city, units) {
-
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=222e451b939e0663df968edf4cecbd01&units=${units}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiID}&units=${units}`)
         .then(response => {
-
 
             if (response.ok) {
                 return (response.json())
-            }else{
-                throw new error ("The call was not completed succesfully")
+            } else {
+                throw new error("The call was not completed succesfully")
             }
 
 
         })
         .then(data => {
-            jsonObjectParse(data)
+            jsonHandler(data)
             return data
         })
         .catch(error => console.log('error', error));
 
 }
 
-function jsonObjectParse(data) {
+function jsonHandler(data) {
     console.log(data)
+    TempDataStore = data
+    let city = data.name
+    let temp = data.main.temp
     let container = document.getElementById("container")
     let element = document.createElement("p")
     container.appendChild(element)
-    element.innerText = JSON.stringify(data)
+    element.innerHTML = (`City: ${city},${data.sys.country} <br> Tempreture: ${temp} F <br> WindSpeed: ${data.wind.speed} MPH`)
 
 
+}
+
+function fiveDayForcast(){
+    
 }
